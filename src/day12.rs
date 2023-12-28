7e7c20e6f9abb99c9fbd927674339c9a83b5549a
HanamiYuzu <txw5ifd9@duck.com>
2023-12-28 16:48:13 +0800

    optimize day12 part1

--- src/day12.rs
+++ src/day12.rs
@@ -1,56 +1,83 @@
+use std::{collections::HashMap, time::Instant};
+
 pub fn day12() {
     let str = include_str!("../day12.txt");
-
+    let timer = Instant::now();
     let total = part1(str);
-    println!("total: {}", total);
+    println!("total: {} in {:?}", total, timer.elapsed());
 }
 
-fn part1(str: &str) -> i32 {
+fn part1(str: &str) -> u64 {
     parse(str)
-        .into_iter()
-        .map(|(mut pattern, counts)| backtrack(&mut pattern, &counts))
+        .iter()
+        .map(|(pattern, counts)| collect(pattern.as_bytes(), &counts))
         .sum()
 }
 
-fn parse(str: &str) -> Vec<(String, Vec<i32>)> {
+fn parse(str: &str) -> Vec<(&str, Vec<i32>)> {
     str.lines()
         .filter_map(|l| {
-            l.split_once(' ').map(|u| {
-                (
-                    u.0.to_string(),
-                    u.1.split(',').map(|n| n.parse().unwrap()).collect(),
-                )
-            })
+            l.split_once(' ')
+                .map(|u| (u.0, u.1.split(',').map(|n| n.parse().unwrap()).collect()))
         })
         .collect()
 }
 
-fn is_valid(pattern: &str, counts: &[i32]) -> bool {
-    pattern
-        .split('.')
-        .filter(|x| !x.is_empty())
-        .map(|x| x.chars().count() as i32)
-        .collect::<Vec<_>>()
-        == counts
+fn collect(pattern: &[u8], sizes: &[i32]) -> u64 {
+    collect_with_cache(pattern, sizes, 0, 0, 0, &mut HashMap::new())
 }
 
-fn backtrack(pattern: &mut String, counts: &[i32]) -> i32 {
-    match pattern.find('?') {
-        Some(i) => {
-            unsafe {
-                pattern.as_bytes_mut()[i] = b'#';
+fn collect_with_cache(
+    pattern: &[u8],
+    sizes: &[i32],
+    i: usize,
+    j: usize,
+    cur_size: i32,
+    cache: &mut HashMap<(usize, usize, i32), u64>,
+) -> u64 {
+    if i >= pattern.len() {
+        return match j {
+            _ if j >= sizes.len() => 1,
+            _ if j == sizes.len() - 1 && sizes[j] == cur_size => 1,
+            _ => 0,
+        };
+    }
+
+    match pattern[i] {
+        b'?' => {
+            if let Some(&n) = cache.get(&(i, j, cur_size)) {
+                return n;
             }
-            let a = backtrack(pattern, counts);
-            unsafe {
-                pattern.as_bytes_mut()[i] = b'.';
+            let mut total = 0;
+            if cur_size == 0 {
+                total += collect_with_cache(pattern, sizes, i + 1, j, 0, cache);
             }
-            let b = backtrack(pattern, counts);
-            unsafe {
-                pattern.as_bytes_mut()[i] = b'?';
+            if let Some(&size) = sizes.get(j) {
+                if cur_size < size {
+                    total += collect_with_cache(pattern, sizes, i + 1, j, cur_size + 1, cache);
+                } else {
+                    total += collect_with_cache(pattern, sizes, i + 1, j + 1, 0, cache);
+                }
             }
-            a + b
+            cache.insert((i, j, cur_size), total);
+            return total;
         }
-        None => is_valid(pattern, counts) as i32,
+        b'#' => {
+            if j >= sizes.len() || cur_size + 1 > sizes[j] {
+                return 0;
+            }
+            return collect_with_cache(pattern, sizes, i + 1, j, cur_size + 1, cache);
+        }
+        b'.' => {
+            if cur_size == 0 {
+                return collect_with_cache(pattern, sizes, i + 1, j, 0, cache);
+            }
+            if j >= sizes.len() || cur_size != sizes[j] {
+                return 0;
+            }
+            return collect_with_cache(pattern, sizes, i + 1, j + 1, 0, cache);
+        }
+        _ => panic!(),
     }
 }
 
@@ -59,7 +86,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1_1() {
+    fn test_day12_1() {
         let str = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -67,5 +94,11 @@ mod tests {
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1";
         assert_eq!(part1(str), 21);
+    }
+
+    #[test]
+    fn test_day12_2() {
+        let str = include_str!("../day12.txt");
+        assert_eq!(part1(str), 7705);
     }
 }
