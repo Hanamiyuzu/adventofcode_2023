c3f64fb938dad01cc6aad121167fe34c33febf8e
HanamiYuzu <txw5ifd9@duck.com>
2024-01-02 19:47:21 +0800

    day16

---
+++ src/day16.rs
@@ -0,0 +1,119 @@
+use std::collections::HashSet;
+
+pub fn day16() {
+    let str = include_str!("../day16.txt");
+    let timer = std::time::Instant::now();
+    let total = part1(str);
+    println!("total: {} in {:?}", total, timer.elapsed());
+}
+
+fn part1(str: &str) -> i32 {
+    let map = parse(str);
+    let mut visited = vec![vec![false; map[0].len()]; map.len()];
+    let mut cache = HashSet::with_capacity(4 * map.len() * map[0].len());
+
+    refract(&map, (0, 0, DIR::RIGHT), &mut visited, &mut cache);
+
+    visited.iter().flatten().filter(|&&x| x).count() as i32
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum DIR {
+    TOP,
+    RIGHT,
+    BOTTOM,
+    LEFT,
+}
+
+fn refract(
+    map: &[Vec<char>],
+    (row, col, dir): (i32, i32, DIR),
+    visited: &mut Vec<Vec<bool>>,
+    cache: &mut HashSet<(i32, i32, DIR)>,
+) {
+    if row < 0 || row >= map.len() as i32 || col < 0 || col >= map[0].len() as i32 {
+        return;
+    }
+    if cache.contains(&(row, col, dir)) {
+        return;
+    }
+    cache.insert((row, col, dir));
+    visited[row as usize][col as usize] = true;
+    match map[row as usize][col as usize] {
+        '.' => {
+            refract(map, to_next(row, col, dir), visited, cache);
+        }
+        '/' => {
+            let dir = match dir {
+                DIR::TOP => DIR::RIGHT,
+                DIR::RIGHT => DIR::TOP,
+                DIR::BOTTOM => DIR::LEFT,
+                DIR::LEFT => DIR::BOTTOM,
+            };
+            refract(map, to_next(row, col, dir), visited, cache);
+        }
+        '\\' => {
+            let dir = match dir {
+                DIR::TOP => DIR::LEFT,
+                DIR::RIGHT => DIR::BOTTOM,
+                DIR::BOTTOM => DIR::RIGHT,
+                DIR::LEFT => DIR::TOP,
+            };
+            refract(map, to_next(row, col, dir), visited, cache);
+        }
+        '|' => match dir {
+            DIR::LEFT | DIR::RIGHT => {
+                refract(map, to_next(row, col, DIR::TOP), visited, cache);
+                refract(map, to_next(row, col, DIR::BOTTOM), visited, cache);
+            }
+            _ => refract(map, to_next(row, col, dir), visited, cache),
+        },
+        '-' => match dir {
+            DIR::TOP | DIR::BOTTOM => {
+                refract(map, to_next(row, col, DIR::LEFT), visited, cache);
+                refract(map, to_next(row, col, DIR::RIGHT), visited, cache);
+            }
+            _ => refract(map, to_next(row, col, dir), visited, cache),
+        },
+        _ => panic!(),
+    }
+}
+
+fn to_next(row: i32, col: i32, dir: DIR) -> (i32, i32, DIR) {
+    match dir {
+        DIR::TOP => (row - 1, col, dir),
+        DIR::RIGHT => (row, col + 1, dir),
+        DIR::BOTTOM => (row + 1, col, dir),
+        DIR::LEFT => (row, col - 1, dir),
+    }
+}
+
+fn parse(str: &str) -> Vec<Vec<char>> {
+    str.lines().map(|s| s.chars().collect()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day16_1() {
+        let str = r".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....";
+        assert_eq!(part1(str), 46);
+    }
+
+    #[test]
+    fn test_day16_2() {
+        let str = include_str!("../day16.txt");
+        assert_eq!(part1(str), 8389);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day12;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
 mod day2;
 mod day3;
 mod day4;
@@ -29,5 +30,6 @@ fn main() {
     //day12::day12();
     //day13::day13();
     //day14::day14();
-    day15::day15();
+    //day15::day15();
+    day16::day16();
 }
