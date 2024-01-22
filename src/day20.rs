a0d4b9b0f07804dc47635ac71ee1c6d9e7f21385
HanamiYuzu <txw5ifd9@duck.com>
2024-01-22 10:33:10 +0800

    day20 part2

--- src/day20.rs
+++ src/day20.rs
@@ -1,9 +1,10 @@
+use num_integer::Integer;
 use std::collections::{HashMap, VecDeque};
 
 pub fn day20() {
     let str: &'static str = include_str!("../day20.txt");
     let timer = std::time::Instant::now();
-    let total = part1(str);
+    let total = part2(str);
     println!("total: {} in {:?}", total, timer.elapsed());
 }
 
@@ -51,6 +52,61 @@ fn part1(str: &'static str) -> i64 {
         }
     }
     hc * lc
+}
+
+fn part2(str: &'static str) -> i64 {
+    let mut modules = parse(str);
+    let mut send_hi_to_rs = [0; 4];
+    // I'm trapped here until I realize that my count starts from 0 (for count in 0..)
+    for count in 1.. {
+        let mut q = VecDeque::from([("broadcaster", Pulse::Lo)]);
+        while let Some((n, pulse)) = q.pop_front() {
+            if pulse == Pulse::Hi {
+                match n {
+                    "bt" if send_hi_to_rs[0] == 0 => send_hi_to_rs[0] = count,
+                    "dl" if send_hi_to_rs[1] == 0 => send_hi_to_rs[1] = count,
+                    "fr" if send_hi_to_rs[2] == 0 => send_hi_to_rs[2] = count,
+                    "rv" if send_hi_to_rs[3] == 0 => send_hi_to_rs[3] = count,
+                    _ => {}
+                }
+            }
+            for t in modules.get(n).unwrap().ts() {
+                if t == "rx" || t == "output" {
+                    continue;
+                }
+                let module = modules.get_mut(t).unwrap();
+                if !match module {
+                    Module::Flipflop(..) => pulse == Pulse::Lo,
+                    _ => true,
+                } {
+                    continue;
+                }
+                let np = match module {
+                    Module::Broadcaster(..) => pulse,
+                    Module::Flipflop(p, ..) => {
+                        if pulse == Pulse::Lo {
+                            *p = !*p;
+                        }
+                        *p
+                    }
+                    Module::Conjunction(map, ..) => {
+                        map.insert(n, pulse);
+                        if map.values().all(|&p| p == Pulse::Hi) {
+                            Pulse::Lo
+                        } else {
+                            Pulse::Hi
+                        }
+                    }
+                    _ => panic!(),
+                };
+                q.push_back((t, np));
+            }
+        }
+        if send_hi_to_rs.iter().all(|&c| c > 0) {
+            return send_hi_to_rs.iter().fold(1, |acc, &count| acc.lcm(&count));
+        }
+    }
+    unreachable!();
 }
 
 #[derive(PartialEq, Clone, Copy, Debug)]
@@ -139,5 +195,16 @@ mod tests {
     fn test_day20_2() {
         let str: &'static str = include_str!("../day20.txt");
         assert_eq!(part1(str), 834323022);
+        assert_eq!(part2(str), 225386464601017);
+    }
+
+    #[test]
+    fn test_day20_3() {
+        let str: &'static str = "broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output";
+        assert_eq!(part1(str), 11687500);
     }
 }
